cf6b649f14b33b0cc17a63ab859a7261765611fc
Jefferson Valim <jpvalim@hotmail.com>
2021-09-08 11:37:12 -0300

    Layers

---
+++ backend/src/main/java/com/jpv/dsvendas/controllers/SellerController.java
@@ -0,0 +1,27 @@
+package com.jpv.dsvendas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jpv.dsvendas.dto.SellerDTO;
+import com.jpv.dsvendas.services.SellerService;
+
+@RestController
+@RequestMapping (value="/sellers")
+public class SellerController {
+	
+	@Autowired
+	private SellerService service;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findAll(){
+		List<SellerDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+		
+	}
+}
---
+++ backend/src/main/java/com/jpv/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.jpv.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jpv.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
