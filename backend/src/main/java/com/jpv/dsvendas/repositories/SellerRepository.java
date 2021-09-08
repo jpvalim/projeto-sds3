6ba4bfdb131bfb21a393c1868d3d9ba313f9fcc0
Jefferson Valim <jpvalim@hotmail.com>
2021-09-08 19:33:53 -0300

    Group by search

--- backend/src/main/java/com/jpv/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/jpv/dsvendas/repositories/SellerRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.jpv.dsvendas.entities.Seller;
 
 public interface SellerRepository extends JpaRepository<Seller, Long>{
-
+	
+	
+	
 }
--- backend/src/main/java/com/jpv/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/jpv/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.jpv.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jpv.dsvendas.dto.SaleDTO;
+import com.jpv.dsvendas.dto.SaleSuccessDTO;
+import com.jpv.dsvendas.dto.SaleSumDTO;
 import com.jpv.dsvendas.entities.Sale;
 import com.jpv.dsvendas.repositories.SaleRepository;
 import com.jpv.dsvendas.repositories.SellerRepository;
@@ -27,5 +31,16 @@ public class SaleService {
 		return result.map(x -> new SaleDTO(x));
 		
 	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupBySeller();
+	}
+	
 	
 }
